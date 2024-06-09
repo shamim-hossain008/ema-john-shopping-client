a0e1d32bebbb7b2302eec83c40fe581f338fb51d
shamim-hossain008 <shamim.hossain3573@gmail.com>
2024-06-10 01:51:37 +0800

    first commit

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,29 +1,31 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductsLoader = async () => {
-    const loadedProducts = await fetch('http://localhost:5000/products');
-    const products = await loadedProducts.json();
+  const loadedProducts = await fetch(
+    `${import.meta.env.VITE_BASE_URL}/products`
+  );
+  const products = await loadedProducts.json();
 
-    // if cart data is in database, you have to use async await
-    const storedCart = getShoppingCart();
+  // if cart data is in database, you have to use async await
+  const storedCart = getShoppingCart();
 
-    const savedCart = [];
+  const savedCart = [];
 
-    for (const id in storedCart) {
-        const addedProduct = products.find(pd => pd._id === id);
-        if (addedProduct) {
-            const quantity = storedCart[id];
-            addedProduct.quantity = quantity;
-            savedCart.push(addedProduct);
-        }
+  for (const id in storedCart) {
+    const addedProduct = products.find((pd) => pd._id === id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      savedCart.push(addedProduct);
     }
+  }
 
-    // if you need to send two things
-    // return [products, savedCart]
-    // another options
-    // return { products, cart: savedCart }
+  // if you need to send two things
+  // return [products, savedCart]
+  // another options
+  // return { products, cart: savedCart }
 
-    return savedCart;
-}
+  return savedCart;
+};
 
 export default cartProductsLoader;
